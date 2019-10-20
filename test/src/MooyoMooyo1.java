b36aa20d84c0f4b3dd7c84349bf73891b3622527
stanl_r0ftaff <stanl_r0ftaff@10.0.0.237>
2019-10-19 17:18:20 -0700

    organized imports

--- test/src/MooyoMooyo1.java
+++ test/src/MooyoMooyo1.java
@@ -3,16 +3,7 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.PriorityQueue;
-import java.util.Scanner;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class MooyoMooyo1 {
