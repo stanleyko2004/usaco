09a8923447e19f3f7b820aae1cf628421a4c4ee4
stanl_r0ftaff <stanl_r0ftaff@10.0.0.237>
2019-10-27 11:04:09 -0700

    first commit

--- test/src/Cowjump.java
+++ test/src/Cowjump.java
@@ -5,70 +5,70 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.Comparator;
 import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Cowjump {
-	public static point[] grid;
-	public static ArrayList<point> activeSet;
 	public static void main(String[] args) throws IOException{
 		BufferedReader reader = new BufferedReader(new FileReader("cowjump.in"));
 		int n = Integer.parseInt(reader.readLine());
-		grid = new point[2*n];
+		point[] points = new point[2*n];
+		line[] lines = new line[n];
 		for (int i=0;i<n;i++){
 			StringTokenizer st = new StringTokenizer(reader.readLine());
 			int x1 = Integer.parseInt(st.nextToken());
 			int y1 = Integer.parseInt(st.nextToken());
 			int x2 = Integer.parseInt(st.nextToken());
 			int y2 = Integer.parseInt(st.nextToken());
-			if (x1<x2){
-				grid[2*i] = new point(i,x1,y1,true);
-				grid[2*i+1] = new point(i,x2,y2,false);
-			} else {
-				grid[2*i] = new point(i,x2,y2,true);
-				grid[2*i+1] = new point(i,x1,y1,false);
+			if (x1>x2){
+				int temp = x1;
+				x1 = x2;
+				x2 = temp;
+				temp = y1;
+				y1 = y2;
+				y2 = temp;
 			}
+			points[2*i] = new point(x1,y1,true,i);
+			points[2*i+1] = new point(x2,y2,false,i);
+			lines[i] = new line(x1,y1,x2,y2,i);
 		}
-		Arrays.sort(grid);
 		reader.close();
-		//sweep line
-		activeSet = new ArrayList<point>();
-		for (point i:grid){
-			if (!i.start){
-				int ind = del(i.id);
+		Arrays.sort(points);
+		ArrayList<line> active = new ArrayList<line>();
+		for (int i=0;i<2*n;i++){
+			point cur = points[i];
+			if (cur.start){
+				//find index to insert
+				
+			} else {
+				active.remove(lines[cur.id]);
 			}
 		}
-		BufferedWriter writer = new BufferedWriter(new FileWriter("cowjump.out"));
-		writer.write(Integer.toString(low));
-		writer.close();
+	}
+	public static class line{
+		public int x1,y1,x2,y2,lineId;
+		public line(int x1,int y1,int x2,int y2,int id){
+			lineId = id;
+			this.x1 = x1;
+			this.y1 = y1;
+			this.x2 = x2;
+			this.y2 = y2;
+		}
 	}
 	public static class point implements Comparable<point>{
-		public int id,x,y;
-		public boolean start;
-		public point(int id,int x,int y,boolean start){
-			this.id = id;
+		public int x,y,id;
+		boolean start;
+		public point(int x,int y,boolean start,int id){
 			this.x = x;
 			this.y = y;
 			this.start = start;
+			this.id = id;
 		}
 		public int compareTo(point other){
-			return other.x-this.x;
+			return this.x-other.x;
 		}
-	}
-	public static int del(int id){
-		for (int i=0;i<activeSet.size();i++){
-			if (activeSet.get(i).id==id){
-				activeSet.remove(i);
-				return i;
-			}
-		}
-		return 0;
-	}
-	public static boolean intersect(int s1,int e1,int s2,int e2){
-		if 
 	}
 }
