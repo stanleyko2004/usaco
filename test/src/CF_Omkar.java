2c53edcc8277b2c6eed7fd8134f01ccdb53f31fe
stanl_r0ftaff <stanl_r0ftaff@10.0.0.237>
2020-07-11 08:20:01 -0700

    7/11/2020

---
+++ test/src/CF_Omkar.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class CF_Omkar {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		for (int i=0; i<t; i++){
+			int n = Integer.parseInt(br.readLine());
+			System.out.print("1");
+			for (int j=1; j<n; j++){
+				System.out.print(" 1");
+			}
+			System.out.println();
+		}
+	}
+}
