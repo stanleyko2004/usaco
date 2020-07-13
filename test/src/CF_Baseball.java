1a39701ac4bd439ebd00f03a52aa1dddabc48794
stanl_r0ftaff <stanl_r0ftaff@10.0.0.237>
2020-07-13 10:55:17 -0700

    7/13/2020

---
+++ test/src/CF_Baseball.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class CF_Baseball {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		for (int i=0; i<t; i++){
+			int n = Integer.parseInt(br.readLine());
+			boolean wrong = false;
+			boolean right = false;
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j=1; j<=n; j++){
+				if (Integer.parseInt(st.nextToken()) == j){
+					right = true;
+				} else {
+					wrong = true;
+				}
+			}
+			if (right && wrong){
+				System.out.println(2);
+			} else if (right && !wrong){
+				System.out.println(0);
+			} else {
+				System.out.println(1);
+			}
+		}
+	}
+}
